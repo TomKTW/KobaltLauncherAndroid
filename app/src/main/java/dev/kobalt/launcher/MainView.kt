67f5277e043a0496810b86201de72f8a39980deb
Tom.K <tom@kobalt.dev>
2020-06-28 00:52:19 +0200

    Add checking for installed packages

--- app/src/main/java/dev/kobalt/launcher/MainView.kt
+++ app/src/main/java/dev/kobalt/launcher/MainView.kt
@@ -1,7 +1,10 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "MemberVisibilityCanBePrivate")
 
 package dev.kobalt.launcher
 
+import android.content.Intent
+import android.content.IntentFilter
+import dev.kobalt.core.utility.Broadcaster
 import dev.kobalt.core.utility.WeakReferenceCache
 import dev.kobalt.core.view.NavigationView
 import dev.kobalt.core.view.StackView
@@ -12,17 +15,26 @@ class MainView : StackView(Orientation.Vertical) {
 
     private val contentNavigation: NavigationView = NavigationView()
 
+    private val packageBroadcaster = Broadcaster(application, IntentFilter().apply {
+        addAction(Intent.ACTION_PACKAGE_ADDED)
+        addAction(Intent.ACTION_PACKAGE_REMOVED)
+        addDataScheme("package")
+    })
+
     val packageLayout = WeakReferenceCache {
         PackageView().apply {
+            fun reloadPackages() = application.jobManager.load { packageRecycler.update(it) }
+            val listener: (Broadcaster.Data) -> Unit = { reloadPackages() }
             packageRecycler.onItemTap = { application.launchFromPackage(it.name) }
+            onAttach = { packageBroadcaster.addListener(listener) }
+            onDetach = { packageBroadcaster.removeListener(listener) }
+            reloadPackages()
         }
     }
 
     init {
         add(contentNavigation, width = matchParent, height = matchParent)
-        contentNavigation.currentLayout = packageLayout.value.apply {
-            application.jobManager.load { packageRecycler.update(it) }
-        }
+        contentNavigation.currentLayout = packageLayout.value
     }
 
 }
